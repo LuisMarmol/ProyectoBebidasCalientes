3a820b37b85d44aea269cfa1003c9183b2cf5147
LuisMarmol <luis.marmol@tecnologicoliceocristiano.edu.ec>
2024-05-15 01:57:32 -0500

    Configured main file

--- hotdrinks.js
+++ hotdrinks.js
@@ -6,5 +6,22 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/api/drinks', drinksRoutes);
+
+app.use((req, res, next)=>{
+    const error = new HttpError('Ruta no existente', 404);
+    throw error;
+});
+
+app.use((error, req, res, next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'Error desconocido'});
+});
+
 
 app.listen(3000);
